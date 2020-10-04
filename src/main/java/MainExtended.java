5af9b1dee151aae4d200e2c2aa57231c2fd5a05b
Magda Ciszek <magdac141@wp.pl>
2020-10-04 20:41:56 +0200

    2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit 2 commit

--- src/main/java/MainExtended.java
+++ src/main/java/MainExtended.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MainExtended {
-}
+public class MainExtended {
+    public static void main(String[] args) {
+        String varString1 = "aaaaaaaaaaaaaaaaaaaaaaa";
+        String varString2 = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+        int varInt = 44;
+        double varDouble = 1.85;
+        char varChar = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(varString1);
+        System.out.println(varString2);
+        System.out.println(varInt);
+        System.out.println(varDouble);
+        System.out.println(varChar);
+        System.out.println(isGoodProgrammer);
+    }
+
+
+
+    }
