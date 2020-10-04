1641f07833c64631dfdc0f2e90034f422fceef7b
Magda Ciszek <magdac141@wp.pl>
2020-10-04 19:37:48 +0200

    aaaaaaaa

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Magdalena");
+        System.out.println("42");
+        System.out.println("C");
+    }
+
+}
