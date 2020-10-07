7f5360f79f61b8b1a8f090502e3fcd50c786c6e3
Magda Ciszek <magdac141@wp.pl>
2020-10-07 18:36:01 +0200

    SimpleArray.java

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,14 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+String[] books = new String[5];
+String book3FromTable = "";
+books[0] = "Rich and poor";
+books[1] = "Cinderella";
+books[2] = "Twin Flames";
+books[3] = "Pretty Woman";
+books[4] = "Lovers";
+book3FromTable = books[2];
+System.out.println("Trzeci element tablicy to : " + book3FromTable);
+    }
+}
+
