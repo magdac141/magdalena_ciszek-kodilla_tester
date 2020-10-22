05fcecbe811b9c1292a1d4d93209bcdd903bdd12
Magda Ciszek <magdac141@wp.pl>
2020-10-23 01:19:23 +0200

    Zadanie 2.7

---
+++ kodilla-intro/src/main/java/BookTest.java
@@ -0,0 +1,6 @@
+public class BookTest {
+    public static void main(String[] args) {
+        Book book1 = Book.of("Isaac Asimov", "The Galaxy");
+        Book book2 = Book.of("Adam Mickiewicz", "Dziady");
+    }
+}
